767f84cff9ef9cafaff657a2659d1f14dc68be0f
N-Teddy <teddy.webdev@gmail.com>
2025-11-15 20:35:00 +0100

    add capacitor for mobile app

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.yourcompany.robertcarpart',
+  appName: 'RobertCarPart',
+  webDir: 'dist'
+};
+
+export default config;
