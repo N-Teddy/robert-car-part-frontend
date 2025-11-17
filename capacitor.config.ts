0c3feca13323bbb04b69c36d360a1bc6bb340dd5
Ngangman Teddy <t.ngangman@commonfactor.tech>
2025-11-17 15:49:40 +0100

    replacing capacitor.config.js with capacitor.config.json

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.yourcompany.robertcarpart',
-  appName: 'RobertCarPart',
-  webDir: 'dist'
-};
-
-export default config;
